a41b71d06665325e7dd568697123d09725d8f066
Piotr <pjaczun@gmail.com>
2021-07-07 14:25:34 +0200

    Added namesdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello World! I am Three now';
+  title = 'Hello World! I am Three nowwwwww';
 }
